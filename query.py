e1862a050acc64e06f1c43bc1fbc870be8e7fbfb
AnonyMad <manasdeepdeb@gmail.com>
2017-11-14 03:01:49 +0530

    Updated Functions for image retreival

---
+++ query.py
@@ -0,0 +1,18 @@
+import cv2
+from img_descriptors import img2modihist
+from searcher import search
+
+def find_relevant(query_path,low,high):
+    
+    query_image = cv2.imread(query_path)
+
+    query_seghist_features = img2modihist(query_image)
+
+    retrieve_count = 10
+
+    print("Searching for relevant images.....\n")
+
+    best_seghist = search(query_seghist_features,retrieve_count,low,high,1)
+
+    print(best_seghist)
+
