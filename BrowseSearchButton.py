7813470123a1eeddc3c59854c2e0399b7aa9d472
AnonyMad <manasdeepdeb@gmail.com>
2017-11-15 00:43:04 +0530

    GUI Update. Clear button configured.

--- BrowseSearchButton.py
+++ BrowseSearchButton.py
@@ -4,22 +4,25 @@ from PIL import Image, ImageTk
 from query import find_relevant
 #from minifeature_gen import gen_dataset
 from tkinter import ttk
-#for minifeature
 import os
 import cv2
 from img_descriptors import img2modihist
 
-
+labels = []
 main = tk.Tk()
-main.geometry("900x900")
+main.geometry("1200x900")
 main.resizable()
-
+main.title("Final Year Project")
+main.config(bg = 'black')
 frame1 = tk.Frame(main)
-frame1.pack(padx = 20, pady = 20)
+frame1.pack()
+frame1.config(bg = 'black')
 frame2 = tk.Frame(main)
-frame2.pack()
+frame2.pack(padx = 5, pady = 5)
+frame2.config(bg = 'black')
 frame3 = tk.Frame(main)
-frame3.pack()
+frame3.config(bg = 'black')
+frame3.pack(padx = 5, pady = 5)
 e1 = tk.Entry(frame1)
 e2 = tk.Entry(frame1)
 e1.insert(0,'1')
@@ -29,9 +32,11 @@ e1.pack(side = 'bottom')
 
 label = tk.Label(frame1, image = None)
 label1 = tk.Label(frame1, text = 'Content Based Image Retrieval')
-label1.pack(fill = 'x', ipadx = 30, ipady = 30, padx = 30, pady = 30)
-label1.config(fg = 'white', bg = 'black')
-label2 = tk.Label()
+label1.pack(ipadx = 20, ipady = 10, padx = 10, pady = 10)
+label1.config(font = ('Algerian', 10), fg = 'white', bg = 'black')
+
+
+
 Imagepath = ''
 def chooseFile(type = 'public'):
       global Imagepath
@@ -41,89 +46,40 @@ def chooseFile(type = 'public'):
       if ImageFile != None:
             data = Image.open(ImageFile)
             img = ImageTk.PhotoImage(data)
-            #canvas = tk.Canvas(width=500,height=400)
-            #canvas.create_image(0,0,image = img)
-            #anvas.image = img
-            #canvas.pack()
-            label.config(image = img, bg = 'cyan')
+            label.config(image = img, bg = 'red')
             label.image = img
-                 
-
-      
+                
 
 button = tk.Button(frame1, text = 'Upload image', command = chooseFile)
 button.pack()
 
-label.pack(ipadx = 10, ipady = 10, padx = 5, pady = 5)
-
-
-
+label.pack(ipadx = 3, ipady = 3, padx = 5, pady = 5)
+label.update
 def search():
       
       imgNum = find_relevant(Imagepath, int(e1.get()), int(e2.get()))
+      
       for i in imgNum[:5]:
-            #filenum = line
-            filename = "C:/Users/Santanu PC/Desktop/Corel10k/"+str(i)+".jpg"
+            filename = "C:/MAD/CBIR/Corel10k/"+str(i)+".jpg"
             data = Image.open(filename)
             img = ImageTk.PhotoImage(data)
-            #img = cv2.imread(filepath)
-            #print(img)
-            #cv2.imshow('image', img)
-            #cv2.waitKey(0)
             label2 = tk.Label(frame2, image = None)
-            label2.config(image = img, bg = 'red')
+            labels.append(label2)
+            label2.config(image = img, bg = 'blue')
             label2.image = img
-            label2.pack(side = 'left')
+            label2.pack(side = 'left', ipadx = 3, ipady = 3, padx = 10, pady = 5)
       for i in imgNum[5:10]:
-            #filenum = line
-            filename = "C:/Users/Santanu PC/Desktop/Corel10k/"+str(i)+".jpg"
+            filename = "C:/MAD/CBIR/Corel10k/"+str(i)+".jpg"
             data = Image.open(filename)
             img = ImageTk.PhotoImage(data)
-            #img = cv2.imread(filepath)
-            #print(img)
-            #cv2.imshow('image', img)
-            #cv2.waitKey(0)
-            label2 = tk.Label(frame3, image = None)
-            label2.config(image = img, bg = 'red')
-            label2.image = img
-            label2.pack(side = 'left')
-
-def dataset():
-      gen_dataset(int(e1.get()), int(e2.get()))
+            label3 = tk.Label(frame3, image = None)
+            labels.append(label3)
+            label3.config(image = img, bg = 'blue')
+            label3.image = img
+            label3.pack(side = 'left',  ipadx = 3, ipady = 3, padx = 10, pady = 5)
       
       
-def clear_label():
-      label2.config(image = '')
-      label2.image = None
-            
-button1 = tk.Button(frame1, text = 'Search image', command = search)
-button1.pack(side = 'left', padx = 20, pady = 20)
-
-button2 = tk.Button(frame1, text = 'Generate dataset', command = dataset)
-button2.pack(side = 'left', padx = 20, pady = 20)
-
-button3 = tk.Button(frame1, text = 'Clear images', command = clear_label)
-button3.pack(side = 'left', padx = 20, pady = 20)
-
-
-progress=ttk.Progressbar(main,orient="horizontal",length=200, mode="determinate")
-
-def prog(i,low,high):
-	progress.pack()
-	progress["maximum"]=high-low
-	progress["value"]=i-low+1
-	main.update()
-
-
-
-
-
-
-
-#MINIFEATURE.PY
-
-
-def gen_dataset(low,high):
+def gen_dataset(low, high):
 	
 
     print("Creating Feature Vectors.....\n")
@@ -140,39 +96,41 @@ def gen_dataset(low,high):
     for i in range(low,high):
         if (i-low+1) % step == 0:
             print("%d%% complete\n"%((i-low+1)//step*10))
-        filepath = "C:/Users/Santanu PC/Desktop/Corel10k/" + str(i) + ".jpg"
+        filepath = "C:/MAD/CBIR/Corel10k/" + str(i) + ".jpg"
         image = cv2.imread(filepath)    
         modihist = img2modihist(image)
         modihist = [str(i) for i in modihist]    
         file_seghist.write("%s,%s\n"%(str(i),",".join(modihist)))
         prog(i,low,high)		   
     file_seghist.close()
-	
-
-
-	
-	
-
     print("Feature Vectors created successfully!")
 
 
+def dataset():
+      gen_dataset(int(e1.get()), int(e2.get()))      
+      
+def clear_label():
+      for label in labels:
+            label.destroy()
+            
+button1 = tk.Button(frame1, text = 'Search image', command = search)
+button1.pack(side = 'left', padx = 10, pady = 10)
+
+button2 = tk.Button(frame1, text = 'Generate dataset', command = dataset)
+button2.pack(side = 'left', padx = 10, pady = 10)
+
+button3 = tk.Button(frame1, text = 'Clear images', command = clear_label)
+button3.pack(side = 'left', padx = 10, pady = 10)
 
 
 
 
+progress=ttk.Progressbar(main,orient="horizontal",length=200, mode="determinate")
 
-
-#STATUS BAR
-
-
-
-
-
-
-
-
-
-
-
+def prog(i,low,high):
+	progress.pack()
+	progress["maximum"]=high-low
+	progress["value"]=i-low+1
+	main.update()
 
 main.mainloop()   
